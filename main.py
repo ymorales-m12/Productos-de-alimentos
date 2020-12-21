65c1f70fdd5741f07e54b8415b219d1f5ccc36d5
Yilca Yamilet Yamilet <yilcayamiletm@gmail.com>
2020-12-21 17:59:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+alimentos = {"sal":'1.05',"arroz":'2.98',"leche":'1.45',"azucar":'2.35',"aceite":'4.50',"jugo":'1.00',"crema":'0.90',"salsa de tomate":'1.10',"cafe duran":'3.00',"tuna de vegetales":'1.65',"lenteja":'1.80'}
+
+print(alimentos)
+print(alimentos['sal'])
+print(alimentos['arroz'])
+print(alimentos['leche'])
+print(alimentos['azucar'])
+print(alimentos['aceite'])
+print(alimentos['jugo'])
+print(alimentos['crema'])
+print(alimentos['salsa de tomate'])
+print(alimentos['cafe duran'])
+print(alimentos['tuna de vegetales'])
+print(alimentos['lenteja'])
+
